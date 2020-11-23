b3ee84869badd6212cf5d2f262c5d6bf32a92871
Dima <wushu_dima@mail.ru>
2020-11-23 23:42:40 +0100

    added methods to User, added json-server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-import { User } from "./Models/User";
+import { User } from "./models/User";
 
-const user = new User({ name: "John", age: 20 });
-console.log(user.get("name"));
-console.log(user.get("age"));
+const user = new User({ name: "brand new name rew", age: 493 });
+// user.fetch();
+
+// user.set({ name: "brand new name", age: 493 });
+user.save();
+
+// import axios from "axios";
+
+// axios.post("http://localhost:3000/users", {
+//   name: "myname",
+//   age: 20,
+// });
+
+// import { User } from "./Models/User";
+
+// const user = new User({});
+
+// user.set({ name: "Dima" });
+
+// // console.log(user.get("name"));
+// // console.log(user.get("age"));
+
+// user.on("change", () => {
+//   console.log("change 1");
+// });
+// user.on("change", () => {
+//   console.log("change 2");
+// });
+// user.on("save", () => {
+//   console.log("save 1");
+// });
+
+// user.trigger("change");
+// user.trigger("save");
